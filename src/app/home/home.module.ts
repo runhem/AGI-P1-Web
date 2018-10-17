5bf58ee9dd6f0cd2159197ceea02a24c5a96c2f6
tasola <petter.tk@hotmail.com>
2018-10-17 17:36:43 +0200

    Add basic  profile cards

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,12 +2,11 @@ import { HomeComponent } from './home.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HowItWorksComponent } from './how-it-works/how-it-works.component';
+import { TeamComponent } from './team/team.component';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [HomeComponent, HowItWorksComponent],
-  exports: [HomeComponent]
+  imports: [CommonModule],
+  declarations: [HomeComponent, HowItWorksComponent, TeamComponent],
+  exports: [HomeComponent],
 })
-export class HomeModule { }
+export class HomeModule {}
