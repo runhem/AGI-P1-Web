645d409123f617c6942fd2178b6910523c28a7d2
alexandrarunhem <alexandra@ep.se>
2018-10-18 09:25:36 +0200

    Removed team component from app-module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { TeamComponent } from './team/team.component';
 import { HomeModule } from './home/home.module';
 import { FooterModule } from './footer/footer.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TeamComponent
   ],
   imports: [
     BrowserModule,
