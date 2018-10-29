0c90ff4cfd6c2a5e04d1cfffb17493937af05858
Linnea Holm <a.linnea.holm@gmail.com>
2018-10-29 15:16:23 +0100

    ny komponent and some style

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,11 +4,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HowItWorksComponent } from './how-it-works/how-it-works.component';
 import { TeamComponent } from './team/team.component';
+import { GoalsComponent } from './goals/goals.component';
 
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [HomeComponent, HowItWorksComponent, TeamComponent, ImageCarouselComponent],
+  declarations: [HomeComponent, HowItWorksComponent, TeamComponent, ImageCarouselComponent, GoalsComponent],
   exports: [HomeComponent],
 })
 export class HomeModule {}
