262d5aebb304a9b1f861f106c2eb766f4a94c89d
alexandrarunhem <alexandra@ep.se>
2018-10-30 15:56:56 +0100

    changed to /assets instead of ../../assets

--- src/app/home/image-carousel/image-carousel.component.ts
+++ src/app/home/image-carousel/image-carousel.component.ts
@@ -8,9 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class ImageCarouselComponent implements OnInit {
 
   public imageUrlArray = [
-    '../../assets/img/how-it-works/airdrop.png',
-    '../../assets/img/how-it-works/airdrop.png',
-    '../../assets/img/how-it-works/airdrop.png'
+    '/assets/img/how-it-works/airdrop.png',
+    '/assets/img/how-it-works/airdrop.png',
+    '/assets/img/how-it-works/airdrop.png'
   ];
 
   constructor() { }
