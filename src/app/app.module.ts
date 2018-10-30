20334381aecf18750e1d687698c971935ea4f3a0
alexandrarunhem <alexandra@ep.se>
2018-10-30 15:45:36 +0100

    removed components that arent used

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,12 @@
-import { HomeComponent } from './home/home.component';
-import { HeaderModule } from './header/header.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeModule } from './home/home.module';
-import { FooterModule } from './footer/footer.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HomeModule, HeaderModule, FooterModule],
+  imports: [BrowserModule, HomeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
